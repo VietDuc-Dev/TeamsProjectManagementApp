e519199243d6e26a159294e1715dda00cecbb03a
vietduc-it <levietduc4566@gmail.com>
2025-06-10 09:17:44 +0700

    Current User Endpoint

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import authRoutes from "./auth.route";
+import userRoutes from "./user.route";
+import isAuthenticated from "../middlewares/isAuthenticated.middleware";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
+router.use("/user", isAuthenticated, userRoutes);
 
 export default router;
