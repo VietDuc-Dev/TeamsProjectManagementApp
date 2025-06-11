59b4469264d31cf3ce4076e50464f8471d8d3753
vietduc-it <levietduc4566@gmail.com>
2025-06-11 23:10:11 +0700

    Workspace Endpoints

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,10 +2,14 @@ import express from "express";
 import authRoutes from "./auth.route";
 import userRoutes from "./user.route";
 import isAuthenticated from "../middlewares/isAuthenticated.middleware";
+import workspaceRoutes from "./workspace.route";
+import memberRoutes from "./member.routes";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/user", isAuthenticated, userRoutes);
+router.use("/workspace", isAuthenticated, workspaceRoutes);
+router.use("/member", isAuthenticated, memberRoutes);
 
 export default router;
